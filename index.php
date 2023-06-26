9b6d914ae693391edbf59a73a716597c54bc8e15
Jonathan Nunez <jonadavid13@gmail.com>
2023-06-25 21:08:13 -0400

    Update 3

--- index.php
+++ index.php
@@ -16,20 +16,24 @@ $alertVariant = "success";
 if (isset($_POST)) {
     if (isset($_POST['btnGuardar']) || isset($_POST['btnGuardarComo'])) {
         $nombreArchivo = $_POST['nombreArchivo'];
-        $archivo = "data/" . $nombreArchivo . ".txt";
+        $archivo = $nombreArchivo . ".txt";
+        $ruta = "data/";
+        $archivoRuta = $ruta . $archivo;
 
         if (isset($_POST['notepad'])) {
             $text = $_POST['notepad'];
 
-            if(file_exists($archivo)){
-                if($operarArchivo = fopen($archivo, "w+")){
+            if (file_exists($archivoRuta)) {
+                if ($operarArchivo = fopen($archivoRuta, "w+")) {
                     fwrite($operarArchivo, $text);
+                    fclose($operarArchivo);
                 } else {
                     echo "no se pudo guardar";
                 }
             } else {
-                if($operarArchivo = fopen($archivo, "w+")){
+                if ($operarArchivo = fopen($archivoRuta, "w+")) {
                     fwrite($operarArchivo, $text);
+                    fclose($operarArchivo);
                 } else {
                     echo "no se pudo guardar";
                 }
@@ -40,7 +44,22 @@ if (isset($_POST)) {
         $nombreArchivo = $nombreInicial;
     }
     if (isset($_POST['btnAbrir'])) {
-        $nombreArchivo = "Abrir";
+        if(isset($_POST['archivoSeleccionado'])){
+            $filename = $_POST['archivoSeleccionado'];
+            $ruta = "data/";
+            $archivoRuta = $ruta . $filename;
+
+            if (file_exists($archivoRuta)) {
+                if ($operarArchivo = fopen($archivoRuta, "r")) {
+                    $text = fread($operarArchivo, filesize($archivoRuta));
+                    fclose($operarArchivo);
+
+                    $nombreArchivo = $filename;
+                } else {
+                    echo "No se pudo abrir el archivo: ", $filename;
+                }
+            } 
+        }
     }
 }
 
@@ -64,12 +83,13 @@ if (isset($_POST)) {
         <h4 class="header-title">Bloc de Notas Online</h4>
         <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>" class="header-buttons d-flex px-2">
             <button type="submit" name="btnNuevo" class="mx-2 btn btn-success">Nuevo archivo</button>
-            <button type="submit" name="btnAbrir" class="mx-2 btn btn-secondary">Abrir archivo</button>
+            <button type="submit" name="btnOpen" class="mx-2 btn btn-secondary">Abrir archivo</button>
         </form>
     </header>
     <main class="main-container d-flex my-3 justify-content-center">
-        <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>" class="notepad-container container-xxl row justify-content-between mx-2">
-            
+        <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>"
+            class="notepad-container container-xxl row justify-content-between mx-2">
+
             <fieldset class="notepad-card card px-0 col-7">
                 <div class="card-header">
                     <span class="text-muted fw-semibold">
@@ -90,41 +110,62 @@ if (isset($_POST)) {
             </fieldset>
             <fieldset class="control-panel card col-2 px-0 col-4">
                 <div class="card-header">
-                    <span class="fw-semibold" >Opciones</span>
+                    <span class="fw-semibold">Opciones</span>
                 </div>
                 <div class="card-body">
                     <div class="panel-body">
                         <span class="dir-card-title">Directorio: </span>
                         <div class="card dir-container mb-2 mt-2">
                             <div class="card-body dir-content">
-                                <?php
-                                $ruta = "data";
-                                $files = scandir($ruta);
-                                foreach ($files as $archivo) {
-                                    echo $archivo; ?>
-                                    <br>
+                                <ul class="lista-archivos ps-1">
                                     <?php
-                                }
-                                ?>
+                                    $ruta = "data";
+                                    $files = scandir($ruta);
+                                    foreach ($files as $archivo) {
+                                        if ($archivo == "." || $archivo == "..") {
+                                            echo '<li class="archivo-lista d-flex justify-content-between align-items-center mb-2">
+                                            <span>', $archivo, '</span>
+                                            </li>';
+                                        } else {
+                                            echo '<li class="archivo-lista d-flex justify-content-between align-items-center mb-2">
+                                                <span>',$archivo,'</span>
+                                                <div class="d-flex">';
+                                            ?> 
+                                                <form id="OpcionArchivo" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+                                            <?php
+                                            echo '<label for=',$archivo.'-id',' class="valor-invisible">',$archivo."-label",'</label>
+                                            <input type="text" id=',$archivo.'-id',' name="archivoSeleccionado" class="valor-invisible"
+                                                        value= ',$archivo,'>
+                                                    <button type="submit" name="btnAbrir"
+                                                        class="btn btn-success me-2">Abrir</button>
+                                                    <button type="submit" name="btnEliminar"
+                                                        class="btn btn-danger">Eliminar</button>
+                                                    </form>
+                                                </div>
+                                            </li>';
+                                        }
+                                    }
+                                    ?>
+                                </ul>
                             </div>
                         </div>
                         <div class="mb-3">
                             <label for="nombreArchivo" class="form-label">Nombre: </label>
-                            <?php 
-                            
-                                
-                                if($nombreArchivo != $nombreInicial){
-                                    if(isset($_POST['btnGuardarComo'])){
-                                        echo '<input type="text" class="form-control" name="nombreArchivo" id="nombreArchivo"
-                                        placeholder="Nombre del archivo" autofocus value=',$nombreArchivo,'>';
-                                    } else {
-                                        echo '<input type="text" class="form-control disabled-input" name="nombreArchivo" id="nombreArchivo"
-                                        placeholder="Nombre del archivo" readonly value=',$nombreArchivo,'>';
-                                    }
-                                } else {
+                            <?php
+
+
+                            if ($nombreArchivo != $nombreInicial) {
+                                if (isset($_POST['btnGuardarComo'])) {
                                     echo '<input type="text" class="form-control" name="nombreArchivo" id="nombreArchivo"
-                                    placeholder="Nombre del archivo" required>';
+                                        placeholder="Nombre del archivo" autofocus value=', $nombreArchivo, '>';
+                                } else {
+                                    echo '<input type="text" class="form-control disabled-input" name="nombreArchivo" id="nombreArchivo"
+                                        placeholder="Nombre del archivo" readonly value=', $nombreArchivo, '>';
                                 }
+                            } else {
+                                echo '<input type="text" class="form-control" name="nombreArchivo" id="nombreArchivo"
+                                    placeholder="Nombre del archivo" required>';
+                            }
                             ?>
                         </div>
                     </div>
